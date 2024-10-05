8a4db2b8e4533c28e8f0d13fa595f89be2500303
Acsel323 <saminaanna1@ya.ru>
2024-10-05 15:09:40 +0300

    Update index.js

--- sweet-pond-ab19/src/index.js
+++ sweet-pond-ab19/src/index.js
@@ -5,9 +5,9 @@ export default {
       return new Response(null, {
         status: 204,
         headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'POST, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type'
+          'Access-Control-Allow-Origin': '*',  // Разрешаем доступ с любых источников
+          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',  // Разрешаем методы
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization'  // Разрешаем заголовки
         }
       });
     }
@@ -17,37 +17,35 @@ export default {
       const API_KEY = MAILCHIMP_API_KEY;       // Mailchimp API-ключ
       const AUDIENCE_ID = MAILCHIMP_AUDIENCE_ID; // ID аудитории (списка)
 
-      const data = {
-        email_address: email,
-        status: 'subscribed'  // Статус подписки
-      };
-
       const response = await fetch(`https://us11.api.mailchimp.com/3.0/lists/${AUDIENCE_ID}/members`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `apikey ${API_KEY}`
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify({
+          email_address: email,
+          status: 'subscribed'  // Статус подписки
+        })
       });
 
       const headers = {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type'
+        'Access-Control-Allow-Origin': '*',  // Разрешаем доступ с любых источников
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
       };
 
       if (response.ok) {
-        return new Response(JSON.stringify({ success: true, message: 'Вы подписаны!' }), { status: 200, headers });
+        return new Response(JSON.stringify({ success: true, message: 'Success' }), { status: 200, headers });
       } else {
         const errorData = await response.json();
         console.error('Ошибка при подписке:', errorData);
-        return new Response(JSON.stringify({ success: false, message: 'Ошибка при подписке.' }), { status: 500, headers });
+        return new Response(JSON.stringify({ success: false, message: 'Error.' }), { status: 500, headers });
       }
     } catch (error) {
       console.error('Ошибка сервера:', error);
-      return new Response(JSON.stringify({ success: false, message: 'Ошибка сервера.' }), { status: 500, headers });
+      return new Response(JSON.stringify({ success: false, message: 'Server error.' }), { status: 500, headers });
     }
   }
 };
